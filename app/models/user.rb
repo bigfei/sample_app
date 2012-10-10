5cbeb67f2a80e3ce98fc7bd4b04dde9a6c577f12
Felix Hsu <xuchenfei@gmail.com>
2012-10-10 09:54:56 +0800

    Finish Exercise for Chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: {case_sensitive: false}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
   #before_save { |user| user.email = email.downcase }
